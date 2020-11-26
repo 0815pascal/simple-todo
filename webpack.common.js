500ef540ad0a94a0918b755c8e71f279f4725c72
Pascal Witzig <pascal.witzig@gmail.com>
2020-11-26 15:35:15 +0100

    fix build issue

--- webpack.common.js
+++ webpack.common.js
@@ -9,12 +9,7 @@ const rules = [
 
 module.exports = {
   target: 'web', 
-  mode: 'development', 
   entry: './src/index.tsx',
-  output: {
-    path: path.resolve(__dirname, 'build'), 
-    filename: 'bundle.js'
-  },
   module: {rules}, 
   resolve: {extensions: ['.ts', '.tsx', '.js']},
   devServer: {
